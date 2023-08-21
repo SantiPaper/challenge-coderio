63bdc7fe4b2448d8903a1543a7a21206c2164810
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-08-21 20:21:29 -0300

    add eslint

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
-const express = require("express")
+const express = require('express');
 
 const server = express();
 
+server.get('/', (req, res) => {
+
+});
+
 server.listen(3001, () => {
-    console.log("Servidor escuchando en http://localhost:3001")
-})
+    console.log('Servidor escuchando en http://localhost:3001');
+});
